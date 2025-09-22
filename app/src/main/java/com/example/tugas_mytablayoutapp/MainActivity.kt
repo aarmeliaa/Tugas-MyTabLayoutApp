7082070f069e78c66173c5ff5df2ed6983cae391
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-09-22 22:07:56 +0700

    DialogExit sudah. tidak pakai ActionBar jadinya pakai ToolBar

--- app/src/main/java/com/example/tugas_mytablayoutapp/MainActivity.kt
+++ app/src/main/java/com/example/tugas_mytablayoutapp/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
 
         val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
--- app/src/main/java/com/example/tugas_mytablayoutapp/WelcomeActivity.kt
+++ app/src/main/java/com/example/tugas_mytablayoutapp/WelcomeActivity.kt
@@ -1,10 +1,34 @@
 package com.example.tugas_mytablayoutapp
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.example.tugas_mytablayoutapp.DialogExit
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val toolbar = findViewById<Toolbar>(R.id.welcome_toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_welcome, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                val dialogExit = DialogExit()
+                dialogExit.show(supportFragmentManager, "exit_dialog")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
