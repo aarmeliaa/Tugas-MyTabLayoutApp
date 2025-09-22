98693d76f36ac03b0e6fd2d609e8c0e877bc8547
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-09-22 14:14:16 +0700

    TabLayout, Intent ke WelcomeActivity, dan berpindah antar TabLayout sudah bisa

--- app/src/main/java/com/example/tugas_mytablayoutapp/MainActivity.kt
+++ app/src/main/java/com/example/tugas_mytablayoutapp/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.tugas_mytablayoutapp
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
+
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Register"
+                1 -> "Login"
+                else -> ""
+            }
+        }.attach()
     }
 }
